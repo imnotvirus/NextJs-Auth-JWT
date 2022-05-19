972a93d8683ee3829c234134ca423cfe67a059e7
Luiz Claudio <lcluizclaudio96@gmail.com>
2022-05-19 20:55:55 -0300

    removed from the root

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,34 @@
+import type { GetServerSideProps, NextPage } from "next";
+import { getApiClient } from "../../services/axios";
+
+type dashboardProps = {
+  user: {
+    id: string;
+    email: string;
+    name: string;
+  };
+};
+
+const pages: NextPage<dashboardProps> = ({ user }) => {
+  return (
+    <section className="">
+      <h1>dashboard</h1>
+      <p>olá, {user?.name}</p>
+    </section>
+  );
+};
+export default pages;
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const apiClient = getApiClient(ctx);
+  const response = await apiClient.get("/profile");
+  return {
+    props: {
+      user: {
+        name: response.data.name,
+        email: response.data.email,
+        id: response.data.id,
+      },
+    },
+  };
+};
